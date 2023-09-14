0f9d3cff3f75d5aad3e87eb5055464fcfde1394c
Chanaka Weerawardena <Chanaka.Weerawardena@assette.com>
2023-09-14 13:46:41 +0530

    remove

--- Code/sud_vs_block3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import datetime as datetime
-
-def sud_vs_block3(params):
-    #This is a python Test for v-intigration
-    testdict = {"MyTest":123,"Mytest2":456}
-    #response["data"] = testdict
-    return testdict
