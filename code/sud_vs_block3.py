55ba0b0975ad2cac4a7aeaa9ac6331232e9b02f2
Chanaka Weerawardena <Chanaka.Weerawardena@assette.com>
2023-09-14 13:47:10 +0530

    adding

---
+++ code/sud_vs_block3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import datetime as datetime
+
+def sud_vs_block3(params):
+    #This is a python Test for v-intigration
+    testdict = {"MyTest":123,"Mytest2":456}
+    #response["data"] = testdict
+    return testdict
